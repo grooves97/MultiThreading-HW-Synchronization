62babc4641b8268bc4e86100b91f8e6662bbaebc
Aseka <grooves97@gmail.com>
2019-06-13 14:05:12 +0600

    Sync and no sync class

---
+++ BankAccount/BankAccount/BankAccountSync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    [Synchronization]
+    public class BankAccountSync : ContextBoundObject
+    {
+        public decimal Sum { get; set; }
+        public void IncreaseBalance(object income)
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine(currentThread.Name + " начал свою работу");
+            Sum += decimal.Parse(income.ToString());
+            Thread.Sleep(5 * new Random().Next(100));
+            Console.WriteLine(Sum);
+        }
+        public void DecreaseBalance(object expenditure)
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine(currentThread.Name + " начал свою работу");
+            Sum -= decimal.Parse(expenditure.ToString());
+            Thread.Sleep(5 * new Random().Next(100));
+            Console.WriteLine(Sum);
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankUser = new BankAccount()
+            {
+                Sum = 10000
+            };
+
+            var threads = new Thread[40];
+
+
+            for (int i = 0, j = 39; i < 20; i++, j--)
+            {
+                var threadIncome = new Thread(bankUser.IncreaseBalance);
+                var threadSpend = new Thread(bankUser.DecreaseBalance);
+                threadIncome.Name = $"Номер потока - {threadIncome.ManagedThreadId}";
+                threadSpend.Name = $"Номер потока - {threadSpend.ManagedThreadId}";
+                threads[i] = threadIncome;
+                threads[j] = threadSpend;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start(new Random().Next(1000));
+            }
+            Console.ReadLine();
+        }
+    }
+}
